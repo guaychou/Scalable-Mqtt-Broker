bcf69319da1209d0e3497d37e50fe0e65a30c710
guaychou <crossmajor99@gmail.com>
2019-11-07 21:41:44 +0700

    add some command

--- terraforrm-resource/script-plan-apply.sh
+++ terraforrm-resource/script-plan-apply.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+terraform plan
+terraform apply --auto-approve
 terraform output kubeconfig>~/.kube/config
 terraform output config_map_aws_auth > configmap.yml
 kubectl apply -f configmap.yml
