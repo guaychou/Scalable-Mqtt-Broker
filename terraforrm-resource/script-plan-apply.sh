d47a1dccabfbe3eec24b384251f04ec5fed1a40d
guaychou <crossmajor99@gmail.com>
2019-11-08 09:13:09 +0700

    renaming output file

--- terraforrm-resource/script-plan-apply.sh
+++ terraforrm-resource/script-plan-apply.sh
@@ -2,5 +2,5 @@
 terraform plan
 terraform apply --auto-approve
 terraform output kubeconfig>~/.kube/config
-terraform output config_map_aws_auth > configmap.yml
-kubectl apply -f configmap.yml
+terraform output config_map_aws_auth > configmap_map_aws_auth.yml
+kubectl apply -f configmap_map_aws_auth.yml
